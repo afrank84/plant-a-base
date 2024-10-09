31aad7df21f40626155f24086d19e6b61c4ccd4e
Anthony Frank <afrank84@yahoo.com>
2024-10-09 10:38:20 -0400

    footer.php - converted from js to php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,27 @@
-
+<footer class="container mt-5">
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+            <p>&copy; <?php echo date("Y"); ?> Plant-a-base. All rights reserved.</p>
+        </div>
+        <div class="col-md-6">
+            <ul class="list-inline text-md-end mb-0">
+                <li class="list-inline-item">
+                    <a href="https://www.instagram.com/afrank84" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-instagram fa-lg"></i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="https://github.com/afrank84" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-github fa-lg"></i>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="https://x.com/afrank84" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-x-twitter fa-lg"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
