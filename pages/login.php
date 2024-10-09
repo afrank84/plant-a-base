e03aaf3830ca4b2406038221cfbb8df5f3436057
Anthony Frank <afrank84@yahoo.com>
2024-10-09 10:47:59 -0400

    login.php - turned of redirect for now

--- pages/login.php
+++ pages/login.php
@@ -3,10 +3,12 @@ require_once '../includes/db_connection.php';
 session_start();
 
 // Redirect if already logged in
+/*
 if (isset($_SESSION['user_id'])) {
     //header("Location: dashboard.php");
     exit();
 }
+*/
 
 $error = '';
 $success = '';
