ca4561d069b163236def74ebceeea9a784db99d7
Anthony Frank <afrank84+github@protonmail.com>
2025-02-15 09:54:11 -0500

    Added a link to php version of site

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ function createMenu() {
     const menuHTML = `
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
             <div class="container-fluid">
-                <a class="navbar-brand fw-bold" href="#"><i class="fas fa-leaf me-2"></i>Plant-a-base</a>
+                <a class="navbar-brand fw-bold" href="./pages/index.php"><i class="fas fa-leaf me-2"></i>Plant-a-base</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
